5a52f703b32ad407e845f6a1f793aaf4ce12d469
gabo <gabriel.gaboardi13@gmail.com>
2024-03-24 18:18:00 -0300

    updated shiki theme to wildberries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import wildberriesTheme from "./shiki-wildberries.json";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(), 
+    tailwind(),
     icon({
       include: {
-        'tabler': ['download'],
-        'material-symbols': ['open-in-new', 'brush'],
-        'bx': ['brush'],
-        'heroicons-outline': ['color-swatch'],
-        'ep': ['arrow-left-bold', 'arrow-right-bold']
+        tabler: ["download"],
+        "material-symbols": ["open-in-new", "brush"],
+        bx: ["brush"],
+        "heroicons-outline": ["color-swatch"],
+        ep: ["arrow-left-bold", "arrow-right-bold"],
       },
-    })
-  ]
+    }),
+  ],
+  markdown: {
+    shikiConfig: { theme: wildberriesTheme },
+  },
 });
