a4785ceda913fce107b7ff20b1da832a89533ba6
gabo <gabriel.gaboardi13@gmail.com>
2024-06-10 23:33:21 -0300

    elemente based refactor + temp scrot removal

--- src/pages/api/og/[...page].png.ts
+++ src/pages/api/og/[...page].png.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'node:fs';
 import path from 'path';
 import { ImageResponse } from '@vercel/og';
 import { getCollection } from 'astro:content';
-import type { APIRoute, GetImageResult } from 'astro';
+import type { APIRoute } from 'astro';
 import { importImage } from '~/lib/assets';
 import { getImage } from 'astro:assets';
 
@@ -22,7 +22,7 @@ const logoComponent = {
       {
         type: 'img',
         props: {
-          tw: 'inline-block h-full',
+          tw: 'h-full',
           src: 'https://wildberries.style/img/ui/wb_logo.svg',
         },
       },
@@ -46,12 +46,12 @@ const screenshotComponent = async (image: string, url: string) => {
   return {
     type: 'div',
     props: {
-      tw: 'absolute border-t border-l overflow-x-auto border-[#00ffb7] h-full flex right-0 top-0 mt-8 w-1/2',
+      tw: 'absolute border-t border-l border-[#00ffb7] h-full flex right-0 top-0 mt-8 w-1/2',
       children: [
         {
           type: 'img',
           props: {
-            tw: 'min-w-full object-cover',
+            tw: 'min-w-full',
             src: absoluteURL.origin + optimizedImage.src,
             ...optimizedImage.attributes,
           },
@@ -61,87 +61,100 @@ const screenshotComponent = async (image: string, url: string) => {
   };
 };
 
+interface jsx {
+  type: string;
+  props: {
+    style?: { [key: string]: string };
+    tw?: string;
+    children?: jsx[] | string;
+  };
+}
+
+interface el {
+  type: string;
+  tw?: string;
+  style?: { [key: string]: string };
+  children?: jsx[] | string;
+  src?: string;
+}
+
+interface definedEl {
+  tw?: string;
+  style?: { [key: string]: string };
+  children?: jsx[] | string;
+}
+
+const el = ({ type, tw, style, children }: el): jsx => {
+  return {
+    type: type,
+    props: {
+      style: style,
+      tw: tw,
+      children: children,
+    },
+  };
+};
+
+const div = ({ tw, style, children }: definedEl) => {
+  return el({ type: 'div', tw, style, children });
+};
+
+const span = ({ tw, style, children }: definedEl) => {
+  return el({ type: 'span', tw, style, children });
+};
+
 export function getStaticPaths() {
   return ports.map((page) => {
+    const screenshot = page.data.images ? page.data.images[0] : undefined;
     return {
       params: { page: page.slug },
-      props: { page },
+      props: { title: page.data.title, screenshot: screenshot },
     };
   });
 }
 
 export const GET: APIRoute = async ({ props, request }) => {
-  const { page } = props;
-  const { data } = page;
-  const { title, subtitle, images } = data;
+  const { title, screenshot } = props;
 
   // Astro doesn't support tsx endpoints so I'm using React-element objects
-  const html = {
-    type: 'div',
-    props: {
-      tw: 'bg-white w-full h-full flex relative',
-      children: [
-        {
-          type: 'div',
-          props: {
-            tw: 'absolute inset-0 h-full w-full',
-            style: {
-              backgroundImage:
-                'linear-gradient(to right, #00ffb712 1px, #19002e 1px), linear-gradient(to bottom, #00ffb7 2px, #19002e 2px)',
-              backgroundSize: '33px 33px',
-            },
-          },
+  const html = div({
+    tw: 'bg-white w-full h-full flex relative',
+    children: [
+      div({
+        tw: 'absolute inset-0 h-full w-full',
+        style: {
+          backgroundImage:
+            'linear-gradient(to right, #00ffb712 1px, #19002e 1px), linear-gradient(to bottom, #00ffb7 2px, #19002e 2px)',
+          backgroundSize: '33px 33px',
         },
-        {
-          type: 'span',
-          props: {
-            tw: 'flex flex-col absolute bottom-14 left-14 w-10/12 items-start',
+      }),
+      span({
+        tw: 'flex flex-col absolute bottom-14 left-14 w-10/12 items-start',
+        children: [
+          div({
+            tw: 'flex flex-row',
             children: [
-              {
-                type: 'div',
-                props: {
-                  tw: 'flex flex-row',
-                  children: [
-                    logoComponent,
-                    {
-                      type: 'span',
-                      props: {
-                        tw: 'bg-[#19002e] text-[#ff0e82] h-20 text-4xl',
-                        children: [
-                          {
-                            type: 'span',
-                            props: {
-                              tw: 'm-auto ml-3',
-                              children: 'for',
-                            },
-                          },
-                        ],
-                      },
-                    },
-                  ],
-                },
-              },
-              {
-                type: 'span',
-                props: {
-                  tw: 'rounded-3xl px-8 py-5 bg-[#c79bff] mb-4 text-7xl text-[#19002e]',
-                  children: title,
-                },
-              },
-              {
-                type: 'span',
-                props: {
-                  tw: 'text-4xl text-white',
-                  children: subtitle,
-                },
-              },
+              logoComponent,
+              span({
+                tw: 'bg-[#19002e] text-[#ff0e82] h-20 text-4xl',
+                children: [
+                  span({
+                    tw: 'm-auto ml-3',
+                    children: 'for',
+                  }),
+                ],
+              }),
             ],
-          },
-        },
-        images && (await screenshotComponent(images[0], request.url)),
-      ],
-    },
-  };
+          }),
+          span({
+            tw: 'rounded-3xl px-8 py-5 bg-[#c79bff] mb-4 text-7xl text-[#19002e]',
+            children: title,
+          }),
+        ],
+      }),
+      // screenshot && (await screenshotComponent(screenshot, request.url)),
+    ],
+  });
 
   return new ImageResponse(html, {
     width: 1200,
