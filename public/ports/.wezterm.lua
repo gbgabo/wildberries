e1b3594d094c3dfa5ce16804db8e001784f2d3ba
Dimitrios Psaroudakis <73707939+LordWorm1996@users.noreply.github.com>
2025-04-26 11:14:56 +0000

    Update .wezterm.lua

--- public/ports/.wezterm.lua
+++ public/ports/.wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
--- Define your custom color scheme
 config.color_schemes = {
     ['Wildberries'] = {
         foreground = "#ff0e82",
@@ -12,37 +11,32 @@ config.color_schemes = {
         selection_bg = "#240041",
         selection_fg = "#900048",
         ansi = {
-            "#19002e", -- black
-            "#d70040", -- red
-            "#00ffb7", -- green
-            "#ffd700", -- yellow
-            "#399ee6", -- blue
-            "#ff0e82", -- magenta
-            "#00f7e2", -- cyan
-            "#c79bff", -- white
+            "#19002e", 
+            "#d70040", 
+            "#00ffb7", 
+            "#ffd700", 
+            "#399ee6", 
+            "#ff0e82", 
+            "#00f7e2", 
+            "#c79bff",
         },
         brights = {
-            "#2a003f", -- bright black
-            "#f40056", -- bright red
-            "#00ffcc", -- bright green
-            "#ffdf00", -- bright yellow
-            "#58a8f3", -- bright blue
-            "#ff2b99", -- bright magenta
-            "#00ffdb", -- bright cyan
-            "#e0a9ff", -- bright white
+            "#2a003f",
+            "#f40056", 
+            "#00ffcc", 
+            "#ffdf00", 
+            "#58a8f3", 
+            "#ff2b99", 
+            "#00ffdb", 
+            "#e0a9ff", 
         },
     },
 }
 
--- Apply the custom color scheme
 config.color_scheme = 'Wildberries'
-
 config.animation_fps = 60
-
 config.enable_tab_bar = false
-
 config.enable_wayland = true
--- Set font and other preferences
 config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Bold' })
 config.font_size = 12.0
 config.window_background_opacity = 0.7
